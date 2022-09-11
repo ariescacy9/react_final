910d153f558bd20f9f9a2fbb149e037e133fc463
tristan99 <ariescacy9@gmail.com>
2022-09-11 12:05:41 -0500

    menu principal mejorado

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -5,36 +5,33 @@ import './Main.css'
 function Main() {
   return (
     <div className='main'>
-      <div className='main_img'>
-        <div className='video'>
+      <div className='main_description'>
+        <div className='main_description-date'>
+          <h1 className='h1'>Bienvenidos!!!</h1>
+          <div className='datePersnal'>
+            <h2>DATOS PERSONALES</h2>
+            <p>NOMBRES: Cristian Alexander</p>
+            <p>APELLIDOS: Carhuas Yuyale</p>
+            <p>EDAD: 28</p>
           </div>
-            <h1 className='h1'>Bienvenidos!!!</h1>
-              <div className='datePersnal'>
-                <h2>DATOS PERSONALES</h2>
-                <p>NOMBRES: Cristian Alexander</p>
-                <p>APELLIDOS: Carhuas Yuyale</p>
-                <p>EDAD: 28</p>
-              </div>
-
-        <div className='tecnology'>
-          <h3>TECNOLOGIAS</h3>
-          <p>HTML</p>
-          <p>CSS</p>
-          <p>JavaScript</p>
-          <p>React</p>
+          <div className='tecnology'>
+            <h3>TECNOLOGIAS</h3>
+            <p>HTML</p>
+            <p>CSS</p>
+            <p>JavaScript</p>
+            <p>React</p>
+          </div>
         </div>
-
-        <div className='futuro'>
+        <div className='main_description-futuro'>
           <h3>Futuro Full Stack</h3>
           <h4>Actualmente estoy en etapa de preparacion Front End, hacentando cada vez más los conocimientos solidos y poniendo ya en practica lo aprendido</h4>
           <h4>En la parte de BackEnd tambien ya tengo la parte teorica de los conceptos fundamentales, pero aun me falta abarcar mas ese campo y experimentar</h4>
           <h4>a diario voy aprendiendo y poniendo en practica lo aprendido</h4>
           <h4>¡Nunca se deja de aprender!</h4>
         </div>
-
-        <div>
-          <h3>dominio</h3>
-          <div className='tecnologyIcon'>
+      </div>
+      <div className='main_tecnology'>
+        <div className='main_tecnology-icon'>
             <p>
             <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt='react'/>
             <p>React</p>
@@ -48,22 +45,21 @@ function Main() {
               <p>HTML</p>
             </p>
             <p>
-              <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt='css'/>
-              <p>CSS</p>
+              <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt='github'/>
+              <p>GitHub</p>
             </p>
             <p>
               <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" alt='git'/>
               <p>Git</p>
             </p>
             <p>
-              <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt='github'/>
-              <p>GitHub</p>
+              <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt='css'/>
+              <p>CSS</p>
             </p>
             <p>
               <img className='icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" alt='sass'/>
               <p>Saas</p>
             </p>
-          </div>
         </div>
       </div>
     </div>
