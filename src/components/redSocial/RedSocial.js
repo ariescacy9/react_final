5866ac9bd4f1fcc6027c557623cd19dbfbda6587
tristan99 <ariescacy9@gmail.com>
2022-09-16 12:39:24 -0500

    redes sociales

--- src/components/redSocial/RedSocial.js
+++ src/components/redSocial/RedSocial.js
@@ -8,22 +8,22 @@ import React from 'react'
 function RedSocial() {
   return (
     <div className='Principal'>
-        <a href="https://web.whatsapp.com/" target="_blank" rel="noreferrer">
+        <a href="https://api.whatsapp.com/send?phone=972211710" target="_blank" rel="noreferrer">
           <WhatsAppIcon 
             color="success" fontSize="large" sx={{ borderRadius: '8px',
             boxShadow: '0 0 .5rem #1372df '}} />
         </a>
-        <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+        <a href="https://www.facebook.com/cristianalexander.carhuasyuyali/" target="_blank" rel="noreferrer">
           <FacebookIcon
             color="primary" fontSize="large" sx={{ borderRadius: '8px',
             boxShadow: '0 0 .5rem #1372df '}} />
         </a>
-        <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer">
+        <a href="www.linkedin.com/in/cristian-alexander-carhuas-yuyale-2a98a522a" target="_blank" rel="noreferrer">
           <LinkedInIcon
             sx={{ color: '#1003AE',borderRadius: '8px',
             boxShadow: '0 0 .5rem #1372df '}} fontSize="large"/>
         </a>
-        <a href="https://github.com/" target="_blank" rel="noreferrer">
+        <a href="https://github.com/ariescacy9" target="_blank" rel="noreferrer">
           <GitHubIcon
             sx={{ color: 'white',borderRadius: '8px',
             boxShadow: '0 0 .5rem #1372df '}} fontSize="large"/>
